287e34dbcd9de07776df7fd57a4993a3c359a0c8
Lauren Herda <lauren@laurenherda.com>
2013-11-10 01:41:01 -0500

    Tons of stuff!
    It actually works now. Pony moves according to an angle & distance,
    speed calculates (probably buggy), direction is updated (also buggy),
    animation occurs when moving, etc.

--- js/miniponies.coffee
+++ js/miniponies.coffee
@@ -11,23 +11,26 @@ jQuery ->
 
     # plugin defaults
     @defaults = {
-      element     : 'body'        # string, selector for container element
-      divClass    : 'js-mp_pony'  # string, class for pony div
-      walkSpeed   : 600           # number, speed of pony when walking
-      walkEasing  : ''            # string, easing equation for walking pony
-      onWalk      : ->            # Function(pony), called when pony walks
-      onHalt      : ->            # Function(pony), called when pony stops walking
-      restingImg  : '/images/nopony.gif'
+      element     : element
+      divClass    : 'js-mp_pony'            # string, class for pony div
+      stride      : 10                      # number, pixels pony travels per millisecond
+      walkEasing  : 'linear'                      # string, easing equation for walking pony
+      onWalk      : ->                      # Function(pony), called when pony walks
+      onHalt      : ->                      # Function(pony), called when pony stops walking
+      restingFromRightImg: '/images/blank-standing-from right.gif'
+      restingFromLeftImg: '/images/blank-standing-from-left.gif'
+      walkingFromRightImg: '/images/blank-from right.gif'
+      walkingFromLeftImg: '/images/blank-from-left.gif'
       maxMove     : 500
-      minMove     : 50
+      minMove     : 20
     }
 
     # default pony CSS
     ponyCss = 
       display   : 'block'
       position  : 'fixed'
-      top       : 0
-      left      : 0
+      top       : 100
+      left      : 100
       opacity   : 1
       'z-index' : 99999
 
@@ -43,13 +46,26 @@ jQuery ->
     # jQuery version of DOM element attached to the plugin
     @$element = $ element
 
-    @pony = $ '<div>'
+    @origin = 
+      x: 0
+      y: 0
+
+    @bounds = 
+      x: @origin.x + $(window).width()
+      y: @origin.y + $(window).height()
+
+    @$pony = $ '<div>'
 
     # current state
     state = 'hidden'
 
+    # current direction
+    @direction = -1
+
     # set current state
-    @setState = ( _state ) -> state = _state
+    @setState = ( _state ) -> 
+      state = _state
+      console.log(state)
 
     #get current state
     @getState = -> state
@@ -62,23 +78,128 @@ jQuery ->
     @callSettingFunction = ( name, args = [] ) ->
       @settings[name].apply( this, args )
 
-    @move = ->
-      newTopPos = Math.floor(Math.random() * @getSetting('maxMove')) + @getSetting('minMove')
-      newLeftPos = Math.floor(Math.random() * @getSetting('maxMove')) + @getSetting('minMove')
-      console.log @pony.position()
-      @pony.animate
-        top: newTopPos
-        left: newLeftPos
-      , @getSetting('walkSpeed')
+    @polarToCart = (d, a)->
+      {
+        x: d * Math.cos(a)
+        y: d * Math.sin(a)
+      }
+
+    @cartToPolar = (orig, dest)->
+      dx = dest.x - orig.x
+      dy = dest.y - orig.y
+      {
+        r: Math.pow((Math.pow(dx, 2) + Math.pow(dy, 2)), .5) * (180/Math.PI)
+        d: Math.atan(dy / dx)
+      }
+
+    # adds destination coordinates 
+    @getDestinationCoordinates = (distance, angle)->
+      coords = @polarToCart(distance, angle)
+      {
+        x: @$pony.position().left + coords.x
+        y: @$pony.position().top + coords.y
+      }
+
+    @randomVector = ->
+      {
+        dist: Math.floor(Math.random() * 200) + 50
+        angle: Math.floor(Math.random() * 360)
+      }
+
+    # TODO: make this return a random location within the bounds
+    @randomLocationInBounds = ->
+      {
+        x: 100
+        y: 100
+      }
+
+    @getPonySpeed = (dist)->
+      (dist / @getSetting('stride')) * 100
+
+    @recenterPony = ->
+      randLoc = @randomLocationInBounds()
+      currentLoc = @$pony.position()
+      # distance = @cartToPolar({
+      #   x: currentLoc.left
+      #   y: currentLoc.top
+      # }, randLoc).d
+      # @animatePony(randLoc, @getPonySpeed(distance))
+      @animatePony(randLoc, 500)
+
+    # Is the pony within the container bounds?
+    @ponyInBounds = ->
+      pt = @$pony.position().top
+      pl = @$pony.position().left
+      (pt > @origin.y && pt < (@bounds.y - @$pony.height())) && (pl > @origin.x && pl < (@bounds.x - @$pony.width()))
+
+    @randomDirection = -> Math.random() < 0.5 ? -1 : 1
+
+    # Which way is the pony traveling?
+    # Returns 1 for left-to-right, -1 for right-to-left.
+    @getPonyDirection = (current, destination)->
+      if (current.x < destination.x) 
+        return 1
+      else return -1
+
+    @getPonyCoords = ->
+      {
+        x: @$pony.position().left
+        y: @$pony.position().top
+      }
+
+    @setPonyImage = (loc)->
+      @$pony.html('<img src="' + loc + '">')
+
+    # Move the pony
+    @animatePony = (coords, speed)->
+      # TODO: use this function to delegate to a variety of more specific kinds of animations (flyPony, winkPony, etc.)
+      @direction = @getPonyDirection(@getPonyCoords(), coords)
+      console.log(@getPonyCoords().x + ", " + coords.x, @direction)
+      @$pony.animate
+        top: coords.y
+        left: coords.x
+      , {
+          duration: speed
+          easing: @getSetting('walkEasing')
+          start: =>
+            if @direction < 0
+              @setPonyImage(@getSetting('walkingFromRightImg'))
+            else
+              @setPonyImage(@getSetting('walkingFromLeftImg'))
+          done: =>
+            if @direction < 0
+              @setPonyImage(@getSetting('restingFromRightImg'))
+            else
+              @setPonyImage(@getSetting('restingFromLeftImg'))
+
+            if !@ponyInBounds()
+              @recenterPony()
+        }
 
     @init = ->
       @settings = $.extend( {}, @defaults, options )
-      @pony.attr 'class', @getSetting('divClass')
-      @pony.html '<img src="' + @getSetting('restingImg') + '">'
+      @$pony.attr 'class', @getSetting('divClass')
+      @direction = 1
+      if(@direction > 0)
+        @setPonyImage(@getSetting('restingFromLeftImg'))
+      else 
+        @setPonyImage(@getSetting('restingFromRightImg'))
       @$container = $ @getSetting('element')
-      @pony.css(ponyCss)
-      @$container.append @pony
-      @move()
+      @$pony.css(ponyCss)
+      @$container.append @$pony
+      @$pony.on 'mouseenter', =>
+        if @$pony.is(':animated')
+          false
+        else
+          v = @randomVector()
+          coords = @getDestinationCoordinates(v.dist, v.angle)
+          coords = {
+            x: Math.abs(coords.x)
+            y: Math.abs(coords.y)
+          }
+          speed = @getPonySpeed(v.dist)
+          console.log(coords)
+          @animatePony(coords, speed)
       @setState 'ready'
 
     # initialise the plugin
--- js/miniponies.js
+++ js/miniponies.js
@@ -3,21 +3,24 @@
     $.miniPonies = function(element, options) {
       var appearAnimateProperties, disappearAnimateProperties, ponyCss, state;
       this.defaults = {
-        element: 'body',
+        element: element,
         divClass: 'js-mp_pony',
-        walkSpeed: 600,
-        walkEasing: '',
+        stride: 10,
+        walkEasing: 'linear',
         onWalk: function() {},
         onHalt: function() {},
-        restingImg: '/images/nopony.gif',
+        restingFromRightImg: '/images/blank-standing-from right.gif',
+        restingFromLeftImg: '/images/blank-standing-from-left.gif',
+        walkingFromRightImg: '/images/blank-from right.gif',
+        walkingFromLeftImg: '/images/blank-from-left.gif',
         maxMove: 500,
-        minMove: 50
+        minMove: 20
       };
       ponyCss = {
         display: 'block',
         position: 'fixed',
-        top: 0,
-        left: 0,
+        top: 100,
+        left: 100,
         opacity: 1,
         'z-index': 99999
       };
@@ -29,10 +32,20 @@
       };
       this.settings = {};
       this.$element = $(element);
-      this.pony = $('<div>');
+      this.origin = {
+        x: 0,
+        y: 0
+      };
+      this.bounds = {
+        x: this.origin.x + $(window).width(),
+        y: this.origin.y + $(window).height()
+      };
+      this.$pony = $('<div>');
       state = 'hidden';
+      this.direction = -1;
       this.setState = function(_state) {
-        return state = _state;
+        state = _state;
+        return console.log(state);
       };
       this.getState = function() {
         return state;
@@ -46,24 +59,136 @@
         }
         return this.settings[name].apply(this, args);
       };
-      this.move = function() {
-        var newLeftPos, newTopPos;
-        newTopPos = Math.floor(Math.random() * this.getSetting('maxMove')) + this.getSetting('minMove');
-        newLeftPos = Math.floor(Math.random() * this.getSetting('maxMove')) + this.getSetting('minMove');
-        console.log(this.pony.position());
-        return this.pony.animate({
-          top: newTopPos,
-          left: newLeftPos
-        }, this.getSetting('walkSpeed'));
+      this.polarToCart = function(d, a) {
+        return {
+          x: d * Math.cos(a),
+          y: d * Math.sin(a)
+        };
+      };
+      this.cartToPolar = function(orig, dest) {
+        var dx, dy;
+        dx = dest.x - orig.x;
+        dy = dest.y - orig.y;
+        return {
+          r: Math.pow(Math.pow(dx, 2) + Math.pow(dy, 2), .5) * (180 / Math.PI),
+          d: Math.atan(dy / dx)
+        };
+      };
+      this.getDestinationCoordinates = function(distance, angle) {
+        var coords;
+        coords = this.polarToCart(distance, angle);
+        return {
+          x: this.$pony.position().left + coords.x,
+          y: this.$pony.position().top + coords.y
+        };
+      };
+      this.randomVector = function() {
+        return {
+          dist: Math.floor(Math.random() * 200) + 50,
+          angle: Math.floor(Math.random() * 360)
+        };
+      };
+      this.randomLocationInBounds = function() {
+        return {
+          x: 100,
+          y: 100
+        };
+      };
+      this.getPonySpeed = function(dist) {
+        return (dist / this.getSetting('stride')) * 100;
+      };
+      this.recenterPony = function() {
+        var currentLoc, randLoc;
+        randLoc = this.randomLocationInBounds();
+        currentLoc = this.$pony.position();
+        return this.animatePony(randLoc, 500);
+      };
+      this.ponyInBounds = function() {
+        var pl, pt;
+        pt = this.$pony.position().top;
+        pl = this.$pony.position().left;
+        return (pt > this.origin.y && pt < (this.bounds.y - this.$pony.height())) && (pl > this.origin.x && pl < (this.bounds.x - this.$pony.width()));
+      };
+      this.randomDirection = function() {
+        var _ref;
+        return (_ref = Math.random() < 0.5) != null ? _ref : -{
+          1: 1
+        };
+      };
+      this.getPonyDirection = function(current, destination) {
+        if (current.x < destination.x) {
+          return 1;
+        } else {
+          return -1;
+        }
+      };
+      this.getPonyCoords = function() {
+        return {
+          x: this.$pony.position().left,
+          y: this.$pony.position().top
+        };
+      };
+      this.setPonyImage = function(loc) {
+        return this.$pony.html('<img src="' + loc + '">');
+      };
+      this.animatePony = function(coords, speed) {
+        var _this = this;
+        this.direction = this.getPonyDirection(this.getPonyCoords(), coords);
+        console.log(this.getPonyCoords().x + ", " + coords.x, this.direction);
+        return this.$pony.animate({
+          top: coords.y,
+          left: coords.x
+        }, {
+          duration: speed,
+          easing: this.getSetting('walkEasing'),
+          start: function() {
+            if (_this.direction < 0) {
+              return _this.setPonyImage(_this.getSetting('walkingFromRightImg'));
+            } else {
+              return _this.setPonyImage(_this.getSetting('walkingFromLeftImg'));
+            }
+          },
+          done: function() {
+            if (_this.direction < 0) {
+              _this.setPonyImage(_this.getSetting('restingFromRightImg'));
+            } else {
+              _this.setPonyImage(_this.getSetting('restingFromLeftImg'));
+            }
+            if (!_this.ponyInBounds()) {
+              return _this.recenterPony();
+            }
+          }
+        });
       };
       this.init = function() {
+        var _this = this;
         this.settings = $.extend({}, this.defaults, options);
-        this.pony.attr('class', this.getSetting('divClass'));
-        this.pony.html('<img src="' + this.getSetting('restingImg') + '">');
+        this.$pony.attr('class', this.getSetting('divClass'));
+        this.direction = 1;
+        if (this.direction > 0) {
+          this.setPonyImage(this.getSetting('restingFromLeftImg'));
+        } else {
+          this.setPonyImage(this.getSetting('restingFromRightImg'));
+        }
         this.$container = $(this.getSetting('element'));
-        this.pony.css(ponyCss);
-        this.$container.append(this.pony);
-        this.move();
+        this.$pony.css(ponyCss);
+        this.$container.append(this.$pony);
+        this.$pony.on('mouseenter', function() {
+          var coords, speed, v;
+          if (_this.$pony.is(':animated')) {
+            return false;
+          } else {
+            v = _this.randomVector();
+            coords = _this.getDestinationCoordinates(v.dist, v.angle);
+            coords = {
+              x: Math.abs(coords.x),
+              y: Math.abs(coords.y)
+            };
+            speed = _this.getPonySpeed(v.dist);
+            console.log(coords);
+            return _this.animatePony(coords, speed);
+          }
+        });
         return this.setState('ready');
       };
       this.init();
